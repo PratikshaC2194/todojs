8cc247732d2a1841aa15552645086caeb021190f
Vijaykumar77 <degalavijaykumar77@gmail.com>
2022-01-20 20:47:20 +0530

    changes

---
+++ index.js
@@ -0,0 +1,36 @@
+
+
+const tasklist=[]
+
+const addtask=()=>{
+
+
+const name=document.getElementById("name").value;
+console.log(name);
+
+const tempObj={
+
+id:Date.now(),
+taskName:name
+
+
+}
+tasklist.push(tempObj)
+console.log(tasklist)
+
+addtaskonscreen()
+}
+
+function addtaskonscreen(){
+const element=document.createElement("div");
+element.setAttribute("class","child")
+
+element.innerText=tasklist[tasklist.length-1].taskName;
+const existingelement=document.getElementById("parent");
+existingelement.appendChild(element)
+
+
+
+
+
+}
